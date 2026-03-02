8fcf16bbf815106a611d7f2878f14e900e251ca9
UltraEpicLeader100 <stickfigure695@gmail.com>
2026-03-02 16:47:03 -0600

    New Deck

---
+++ decks/mushroom_deck.lua
@@ -0,0 +1,28 @@
+
+SMODS.Back {
+    key = 'mushroom_deck',
+    pos = { x = 4, y = 0 },
+    config = {
+    },
+    loc_txt = {
+        name = 'Mushroom Deck',
+        text = {
+            [1] = 'Start with 52 {C:attention}Brick Blocks{}.'
+        },
+    },
+    unlocked = true,
+    discovered = true,
+    no_collection = false,
+    atlas = 'CustomDecks',
+    apply = function(self, back)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for k, v in pairs(G.playing_cards) do
+                    v:set_ability(G.P_CENTERS['m_adelmod_brickblock'])
+                end
+                G.GAME.starting_deck_size = #G.playing_cards
+                return true
+            end
+        }))
+    end
+}
